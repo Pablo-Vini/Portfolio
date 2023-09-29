ee81dfe4779b08a168bcda52eddef51093cdfa42
Pablo Vinícius <124642141+Pablo-Vini@users.noreply.github.com>
2023-09-29 17:44:08 -0300

    Começando nova página

---
+++ script2.js
@@ -0,0 +1,8 @@
+const myObserver = new IntersectionObserver((entries) => {
+    console.log(entries)
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+
+elements.forEach((element) => myObserver.observe(element))
