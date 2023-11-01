275c4ded626740e992671606695142ae4a9c20c5
Pablo Vinícius <124642141+Pablo-Vini@users.noreply.github.com>
2023-11-01 17:29:18 -0300

    Atualizando Formulario

--- script2.js
+++ script2.js
@@ -12,3 +12,16 @@ const elements = document.querySelectorAll('.hidden')
 
 
 elements.forEach((element) => myObserver.observe(element))
+
+
+
+function Enviar(){
+    var nome = document.getElementById("nomeid")
+    var fone = document.getElementById("foneid")
+    var msg = document.getElementById("msgid")
+
+    if(nome.value != ""){
+        alert("Obrigado " + nome.value + ", sua mensagem foi encaminhada com sucesso.")
+        window.location.reload(true)
+    }
+}
