374bdd41c45176b168b7b5dcdc35ea26794ce53c
Pablo Vinícius <124642141+Pablo-Vini@users.noreply.github.com>
2025-05-21 17:38:41 -0300

    Retirada das imagens do modo claro

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ const translations = {
         menu: "Sobre Mim"
     }
 }
+
+const imagens = ["astronauta", "estrelas", "galaxia1", "galaxia2", "galaxia3"]
+
+
+const chk = document.getElementById("chk")
+const header = document.getElementById("pag-ini")
+const body = document.querySelector('body')
+const textosobremim = document.getElementById('textosobremim')
+const menu = document.getElementById('menu')
+const sobremim = document.getElementById('sobremim')
+const tecnologias = document.getElementById('tecnologias')
+const divtecnologia = document.getElementById('div-tecnologia')
+const projetos = document.getElementById('projetos')
+
+
+chk.addEventListener('change', () =>{
+    chk.classList.toggle('dark');
+    header.classList.toggle('dark');
+    body.classList.toggle('dark');
+    textosobremim.classList.toggle('dark');
+    menu.classList.toggle('dark');
+    sobremim.classList.toggle('dark');
+    tecnologias.classList.toggle('dark');
+    divtecnologia.classList.toggle('dark');
+    projetos.classList.toggle('dark');
+    for (let i = 0; i < imagens.length; i++) {
+        document.getElementById(imagens[i]).setAttribute("style", "visibility: hidden;"); 
+    }
+})
