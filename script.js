125ed60b4cb7ff56eed9ca8fbc9f711e34a964db
Pablo Vinícius <124642141+Pablo-Vini@users.noreply.github.com>
2025-06-04 21:24:10 -0300

    Adição da seção serviços

--- script.js
+++ script.js
@@ -14,11 +14,19 @@ const textosobremim = document.getElementById('textosobremim')
 const menu = document.getElementById('menu')
 const sobremim = document.getElementById('sobremim')
 const tecnologias = document.getElementById('tecnologias')
-const divtecnologia = document.getElementById('div-tecnologia')
 const projetos = document.getElementById('projetos')
 
+const divtecnologia = document.querySelectorAll('.div-tecnologia');
+const titletextproject = document.querySelectorAll('.title-text-project');
 
-chk.addEventListener('change', () =>{
+chk.addEventListener('change', () => {
+    divtecnologia.forEach(div => {
+        div.classList.toggle('dark');
+    });
+    titletextproject.forEach(div => {
+        div.classList.toggle('dark');
+    });
+
     chk.classList.toggle('dark');
     header.classList.toggle('dark');
     body.classList.toggle('dark');
@@ -26,7 +34,6 @@ chk.addEventListener('change', () =>{
     menu.classList.toggle('dark');
     sobremim.classList.toggle('dark');
     tecnologias.classList.toggle('dark');
-    divtecnologia.classList.toggle('dark');
     projetos.classList.toggle('dark');
     for (let i = 0; i < imagens.length; i++) {
         document.getElementById(imagens[i]).setAttribute("style", "visibility: hidden;"); 
